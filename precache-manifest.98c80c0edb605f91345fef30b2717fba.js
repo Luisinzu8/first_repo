da9bb70be428104fbc185e4ae7d736631e82bc2b
Luis Inzunza <luisinzu08@gmail.com>
2020-02-14 00:48:25 -0800

    Updates

---
+++ precache-manifest.98c80c0edb605f91345fef30b2717fba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36ff9ce3d1984d1121a609487b70d235",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "5567b3fca6df5da9d490",
+    "url": "/my-app/static/css/main.2b7842bf.chunk.css"
+  },
+  {
+    "revision": "0be154a3e0c083d1f41f",
+    "url": "/my-app/static/js/2.718ed629.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my-app/static/js/2.718ed629.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5567b3fca6df5da9d490",
+    "url": "/my-app/static/js/main.7a773cb9.chunk.js"
+  },
+  {
+    "revision": "134c6d7b033e4b816dc9",
+    "url": "/my-app/static/js/runtime-main.56c001b7.js"
+  }
+]);
